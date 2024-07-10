ce65a04f1384f2069698019de5b54d2a0ad190cb
Hem Kishore Pradhan <hemkishore009jsr@gmail.com>
2024-07-10 23:13:14 +0530

    Create CrawlerLogFolder.c
    The Leetcode file system keeps a log each time some user performs a change folder operation.
    
    The operations are described below:
    
    "../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
    "./" : Remain in the same folder.
    "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
    You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
    
    The file system starts in the main folder, then the operations in logs are performed.
    
    Return the minimum number of operations needed to go back to the main folder after the change folder operations.Input: logs = ["d1/","d2/","../","d21/","./"]
    Output: 2
    Explanation: Use this change folder operation "../" 2 times and go back to the main folder.Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
    Output: 3
    Example 3:
    
    Input: logs = ["d1/","../","../","../"]
    Output: 0
     
    
    Constraints:
    
    1 <= logs.length <= 103
    2 <= logs[i].length <= 10
    logs[i] contains lowercase English letters, digits, '.', and '/'.
    logs[i] follows the format described in the statement.
    Folder names consist of lowercase English letters and digits.

---
+++ PPT/Functions/CrawlerLogFolder.c
@@ -0,0 +1,15 @@
+int minOperations(char** logs, int logsSize) {
+    int depth = 0;
+    
+    for (int i = 0; i < logsSize; ++i) {
+        if (strcmp(logs[i], "../") == 0) {
+            if (depth > 0) {
+                depth--;
+            }
+        } else if (strcmp(logs[i], "./") != 0) {
+            depth++;
+        }
+    }
+    
+    return depth;
+}
